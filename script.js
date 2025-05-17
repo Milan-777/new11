43a9ad1c45e1213b1969370c0710950ff9b65963
Milan-777 <milankumarhalder95@gmail.com>
2025-05-17 19:48:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//scroll functons...
+function scrol(){
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+window.onscroll= function(){
+    if(window.scrollY > 1000){
+        document.getElementById('gototop').style.display= 'block';
+    }else{
+        document.getElementById('gototop').style.display= 'none';
+    }
+}
+
+//line visibility...
+function line(){
+    document.getElementById('lin1').style.width='16%';
+    document.getElementById('lin2').style.width='16%';
+}
+// if (window.innerWidth <= 600) {
+//         document.getElementById('lin1').style.width = '0%'; // Increase width by 25%
+//         document.getElementById('lin2').style.width = '0%';
+//     }
